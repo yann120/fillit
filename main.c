397f9061c2a389311887fcc494569b2c26d065fa
Yann PETITJEAN <ypetitje@e1r8p3.42.fr>
2019-01-11 18:53:36 +0100

    errors are working

--- main.c
+++ main.c
@@ -3,27 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ypetitje <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ypetitje <ypetitje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 21:53:14 by ypetitje          #+#    #+#             */
-/*   Updated: 2019/01/10 22:01:36 by ypetitje         ###   ########.fr       */
+/*   Updated: 2019/01/11 18:51:57 by ypetitje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fillit.h"
+
+void	print_errors(int error_number)
+{
+	if (error_number == 0)
+		ft_putstr("error\n");
+	else
+		ft_putstr("usage: ./fillit source_file\n");
+}
 int		main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
 		if (!is_valid(argv[1]))
 		{
-			print_error();
+			print_errors(0);
 			return (-1);
 		}
-		fillit(argv[1]);
+		// fillit(argv[1]);
 	}
 	else
-	{
-		print_usage();
-	}
+		print_errors(1);
 	return (0);
 }
--- validation.c
+++ validation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validation.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ypetitje <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ypetitje <ypetitje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 22:02:28 by ypetitje          #+#    #+#             */
-/*   Updated: 2019/01/10 22:03:43 by ypetitje         ###   ########.fr       */
+/*   Updated: 2019/01/11 18:52:28 by ypetitje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@ int		is_valid(char *filepath)
 	// while get next line
 		// if invalid 
 			// return 0
+	(void)filepath; //for testing, to be removed
 	return (1);
 }
