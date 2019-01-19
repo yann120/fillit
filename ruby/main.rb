37b25dfc3f8dd86568cc943e64cada8e31ec6ea5
Flavio Wuensche <fwuensch@student.42.us.org>
2019-01-19 14:08:55 +0100

    Reorganize code resposabilities

--- ruby/main.rb
+++ ruby/main.rb
@@ -2,7 +2,9 @@ require_relative "validation"
 require_relative "pieces"
 require_relative "helper"
 require_relative "fillit"
+require_relative "grid"
 
+init_time = Time.now
 filepath = ARGV[0]
 filetext = File.read(filepath)
 file_is_valid = validate(filetext)
@@ -11,6 +13,7 @@ if file_is_valid
   pieces = read_pieces_from_file(filetext)
   solved_grid = fillit(pieces)
   print_grid(solved_grid)
+  open("logs.out", "a") { |f| f << "#{solved_grid.size},#{Time.now - init_time}\n" }
 else
   print_error(:usage)
 end
